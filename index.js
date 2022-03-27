015854664db5218985fbd8fe7e60c4cf82c78f7c
Andrew Sims <andy@Andrews-MacBook-Pro-2.local>
2022-03-27 18:18:12 -0400

    Added mobile menu

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ import "./src/styles/hero.scss";
 import "./src/styles/home-section.scss";
 import "./src/styles/schedule.scss";
 import "./src/styles/accomodations.scss";
+
+const nav = document.querySelector(".nav-wrapper");
+const body = document.querySelector("body");
+
+function openMenu() {
+    nav.classList.add("menu-open");
+    body.classList.add("menu-open");
+}
+
+function closeMenu() {
+    nav.classList.remove("menu-open");
+    body.classList.remove("menu-open");
+}
+
+const menuButton = document.querySelector(".mobile-menu");
+const closeButton = document.querySelector(".mobile-menu-close-button");
+menuButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
